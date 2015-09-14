e2fcb1dc4daaf05fd888db7278ef24dd56e39fcd
Dylan Gijsbertsen <d.b.gijsbertsen@live.nl>
2015-09-14 14:45:58 +0200

    Fitness Program first start

---
+++ FitnessProgram/app/src/main/java/com/example/dylangijsbertsen/fitnessprogram/Creating_Schedules.java
@@ -0,0 +1,50 @@
+package com.example.dylangijsbertsen.fitnessprogram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class Creating_Schedules extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creating__schedules);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_creating__schedules, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void BackToMainMenu(){
+        Button backButton = (Button) findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Creating_Schedules.this, MainActivity.class));
+            }
+        });
+    }
+}
